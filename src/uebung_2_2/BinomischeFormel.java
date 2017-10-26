85a94bae56d765a0d4dd7a3eed532c8cac8e4c60
Ben6363 <15352@nordakademie.de>
2017-10-26 11:20:42 +0200

    Binomische Formel programmiert.

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,19 @@
+package uebung_2_2;
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a = 8;
+		double b = 3;
+		
+		double binomischeFormel = (a*a) + (2*a*b) + (b*b);
+		
+		System.out.println("Das Ergebnis ist: "+binomischeFormel );
+		
+				
+		
+	}
+
+}
